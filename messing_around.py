f63b6fb13c7515bc075c9792bdb938af0d3d117a
Jessi Shank <jessishank1@gmail.com>
2015-09-18 16:59:31 -0700

    extraneous stuff, sample xml etc

--- messing_around.py
+++ messing_around.py
@@ -16,7 +16,7 @@ class ACS_Catair(ReplaceLines):
             for key, value in acs_line_values.items():
                 self.line_replacement(indi_line,acs_line_values,key)
 
-#this method i think can be used to replace the redundancy in replace_acs_catair. 
+#this function can be used to replace the redundancy in replace_acs_catair. 
 def line_replacement(line,dictionary, key, parsed_section,lines_to_overwrite):
     for line in lines_to_overwrite:
             line = list(line.text)
@@ -24,18 +24,35 @@ def line_replacement(line,dictionary, key, parsed_section,lines_to_overwrite):
             if key = 'AZ' and line[0] == 'A' or line[0] =='Z': 
                 line[dictionary[key]['%s' %i][1]] = dictionary[key]['%s' %i][0]
                 string = ''.join(line)
-                parsed_section.append(string)
             elif key = 'BY' and line[0] == 'B' or line[0] = 'Y':
                 line[dictionary[key]['%s' %i][1]] = dictionary[key]['%s' %i][0]
                 string = ''.join(line)
-                parsed_section.append(string)
             elif line[0]+line[1] = key:
                 line[dictionary[key]['%s' %i][1]] = dictionary[key]['%s' %i][0]
                 string = ''.join(line)
-                parsed_section.append(string)
             else:
                 string = ''.join(line)
-                parsed_section.append(string)
+            parsed_section.append(string)
+
+def line_replacement_by_section(line,dictionary):
+    for key, values in dictionary.items():
+        line = list(line.text)
+        if key == 'AZ' and line[0] == 'A' or line[0] =='Z': 
+            for i in range(1, len(dictionary[key])+1):
+                line[dictionary[key]['%s' %i][1]] = dictionary[key]['%s' %i][0]
+            string = ''.join(line)
+        elif key == 'BY' and line[0] == 'B' or line[0] == 'Y':
+            for i in range(1, len(dictionary[key])+1):
+                line[dictionary[key]['%s' %i][1]] = dictionary[key]['%s' %i][0]
+            string = ''.join(line)
+        elif line[0]+line[1] == str(key):
+            for i in range(1, len(dictionary[key])+1):
+                line[dictionary[key]['%s' %i][1]] = dictionary[key]['%s' %i][0]
+            string = ''.join(line)
+        else:
+            string = ''.join(line)
+        return string
+
 
 for key, val in g.acscatair.iteritems():
     print key
