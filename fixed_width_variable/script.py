dd00c6e03ee46ef5240af2a063699ce070ea6260
Jessi Shank <jessishank1@gmail.com>
2015-10-29 15:26:28 -0700

    nothing of consequence.

--- fixed_width_variable/script.py
+++ fixed_width_variable/script.py
@@ -2,12 +2,16 @@ from grasping_more import *
 import json
 from collections import OrderedDict
 import os
+import xml.etree.ElementTree as etree
+
+sys_input = str(sys.argv[1])
+sys_output = str(sys.argv[2])
 
 cust_map = json.load(open(os.path.join('input','to_json.json')))
-
+cust_map['namespace_usentry']
 #also the json file should just be dumped and ordered by the line value, then reverse ordered by the slice (high -> low)
 
-doc = os.path.join('input','pga_ttb.xml')
+doc = os.path.join('input','%s.xml' % sys_input)
 
 
 parser = etree.XMLParser(encoding='utf-8')
@@ -16,4 +20,4 @@ testCase = etree.parse(doc, parser)
 test = ReplaceItAll(testCase,cust_map['namespace'],cust_map)
 test.register_ns()
 test.ReplaceEverything(cust_map)
-test.rewrite_TestCase(os.path.join('outputs','pga_ttb.xml'))
+test.rewrite_TestCase(os.path.join('outputs','%s.xml' % sys_output))
